e5c5f3c4a496ff89e5eaf756cf84dba5e7ec9ba2
Mohammad Osama <ansariosama024@gmail.com>
2026-02-15 22:23:41 +0530

    Add composite number checker in composite.cpp
    The program checks if a number is composite by testing divisibility.

---
+++ composite.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+    for(int i=2; i<=n/2; i++){
+        if(n%i==0){
+           cout<<"Composite";}
+        else 
+        cout<<"Not composite:";
+           break;
+        }
+    }
+
+
